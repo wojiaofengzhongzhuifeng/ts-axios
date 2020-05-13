8843205cceffac7eab30017a4be2231bf3e28ca6
饶家俊 <raojiajun@bimwinner.com>
2020-05-13 10:03:50 +0800

    处理响应数据 > 获取响应数据

--- examples/simple/app.ts
+++ examples/simple/app.ts
@@ -1,24 +1,21 @@
 import axios from '../../src/ts-axios';
-import { isEmpty } from '../../src/helpers/utils'
 
-console.log(1)
-console.log(isEmpty(null));
-axios({
-  method: 'get',
-  url: '/simple/get#hash?a=1',
-  params: {
-    a: 1,
-    b: [2,3],
-    c: 4,
-    d: {
-      dd: "ddd"
-    },
-    e: new Date(),
-    foo: '@:$, ',
-    zzz: null,
-    yyy: undefined,
-  }
-})
+// axios({
+//   method: 'get',
+//   url: '/simple/get#hash?a=1',
+//   params: {
+//     a: 1,
+//     b: [2,3],
+//     c: 4,
+//     d: {
+//       dd: "ddd"
+//     },
+//     e: new Date(),
+//     foo: '@:$, ',
+//     zzz: null,
+//     yyy: undefined,
+//   }
+// })
 
 axios({
   method: 'post',
@@ -34,9 +31,9 @@ axios({
   }
 })
 
-const arr = new Int32Array([21, 31])
-axios({
-  method: 'post',
-  url: '/base/buffer',
-  data: arr
-})
+// const arr = new Int32Array([21, 31])
+// axios({
+//   method: 'post',
+//   url: '/base/buffer',
+//   data: arr
+// })
--- src/xhr.ts
+++ src/xhr.ts
@@ -3,16 +3,24 @@ import { IAxiosConfig } from './types'
 import { bindUrl, transformRequestData, transformRequestHeaders, setHeader} from './helpers/utils'
 
 function xhr(obj: IAxiosConfig){
-  let {data = null, method = 'get', url, params = {}, headers = {}} = obj;
+  return new Promise((resolve, reject)=>{
+    let {data = null, method = 'get', url, params = {}, headers = {}} = obj;
 
-  url = bindUrl(url, params);
-  data = transformRequestData(data);
-  headers = transformRequestHeaders(headers);
+    url = bindUrl(url, params);
+    data = transformRequestData(data);
+    headers = transformRequestHeaders(headers);
 
-  const request = new XMLHttpRequest();
-  request.open(method, url);
-  setHeader(request, headers);
-  request.send(data);
+    const request = new XMLHttpRequest();
+    request.onreadystatechange = function() {
+      if (request.readyState === 4) {
+        resolve(request);
+      }
+    }
+
+    request.open(method, url);
+    setHeader(request, headers);
+    request.send(data);
+  });
 }
 
 export default xhr;
